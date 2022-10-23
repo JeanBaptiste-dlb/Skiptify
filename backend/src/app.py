3ca35f74e29ea327382045f1a3e9f789e33356e4
noeminellen <48247494+noeminellen@users.noreply.github.com>
2022-10-23 15:53:37 +0200

    connection working

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, redirect, url_for
 from backend.src.spotify_api.api_interface import SPOTIFY_API_INTERFACE
-import pandas as pd
 import numpy as np
 from flask_cors import CORS
 import csv
@@ -29,14 +28,14 @@ def index():
 
 @app.route('/current_info')
 def current_info():
-    data = sp.get_metadata_current()
-    skipped_songs = skipped_song_list()
+    data = sp.get_metadata()
+    #skipped_songs = skipped_song_list()
     if data is not None:
         dict = data.to_dict()
 
     else:
         dict = pd.Series()
-    return {dict, skipped_songs}, 200  # return data and 200 OK code
+    return dict, 200  # return data and 200 OK code
 
 if __name__ == '__main__':
     app.run(debug=True)

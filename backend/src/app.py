311d62f9a19781002a00837e5b9569c34b22e72d
noeminellen <48247494+noeminellen@users.noreply.github.com>
2022-10-23 13:27:22 +0200

    add rest api

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, redirect, url_for
-from backend.src.utils.api_interface import SPOTIFY_API_INTERFACE
+from backend.src.spotify_api.api_interface import SPOTIFY_API_INTERFACE
 
 app = Flask(__name__)
 app.debug = True
 
 sp = SPOTIFY_API_INTERFACE()
 sp.get_current_song()
+
 
 @app.route('/')
 def index():
@@ -14,6 +15,12 @@ def index():
 
 @app.route('/current_info')
 def current_info():
+    data = sp.get_current_song()
+    dict = data.to_dict()
+    return {'data': dict}, 200  # return data and 200 OK code
+
+@app.route('/skipped_songs')
+def skipped_song():
     data = sp.get_current_song()
     dict = data.to_dict()
     return {'data': dict}, 200  # return data and 200 OK code

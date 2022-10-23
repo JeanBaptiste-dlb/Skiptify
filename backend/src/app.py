844aec3be0c1911bb95f4230b64de300f2bd1ad4
noeminellen <48247494+noeminellen@users.noreply.github.com>
2022-10-23 11:43:51 +0200

    use api_interface for info

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,20 +1,22 @@
 from flask import Flask, redirect, url_for
-from backend.src.utils.frontend_info import df
+from backend.src.utils.api_interface import SPOTIFY_API_INTERFACE
 
 app = Flask(__name__)
 app.debug = True
 
+sp = SPOTIFY_API_INTERFACE()
+sp.get_current_song()
 
 @app.route('/')
 def index():
-    return redirect(url_for('welcome'))
+    return redirect(url_for('current_info'))
 
 
-@app.route('/welcome')
-def welcome():
-    data = df.to_dict()
-    return {'data': data}, 200  # return data and 200 OK code
-
+@app.route('/current_info')
+def current_info():
+    data = sp.get_current_song()
+    dict = data.to_dict()
+    return {'data': dict}, 200  # return data and 200 OK code
 
 if __name__ == '__main__':
     app.run(debug=True)

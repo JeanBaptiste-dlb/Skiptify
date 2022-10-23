431a02d8d06fb215da10c71403fd57a0910faed0
noeminellen <48247494+noeminellen@users.noreply.github.com>
2022-10-23 15:42:31 +0200

    blahblah

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,13 +3,23 @@ from backend.src.spotify_api.api_interface import SPOTIFY_API_INTERFACE
 import pandas as pd
 import numpy as np
 from flask_cors import CORS
+import csv
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
 app.debug = True
-
 sp = SPOTIFY_API_INTERFACE()
 sp.get_current_song()
+
+
+def skipped_song_list():
+    df = pd.read_csv('../../data/player_data/skipped.csv')
+    skipped_song_list = pd.DataFrame(index=df['id'].to_list())
+    for id in df['id']:
+        song_info = sp.get_track_metadata(id)
+        skipped_song_list.loc[id] = song_info
+    return skipped_song_list
 
 
 @app.route('/')
@@ -20,11 +30,13 @@ def index():
 @app.route('/current_info')
 def current_info():
     data = sp.get_metadata_current()
+    skipped_songs = skipped_song_list()
     if data is not None:
         dict = data.to_dict()
+
     else:
         dict = pd.Series()
-    return dict, 200  # return data and 200 OK code
+    return {dict, skipped_songs}, 200  # return data and 200 OK code
 
 if __name__ == '__main__':
     app.run(debug=True)
